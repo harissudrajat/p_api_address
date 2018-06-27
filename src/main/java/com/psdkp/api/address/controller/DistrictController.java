085e63b2f0a79643d70c679224ecb73b779db5f6
Matius Apry Prastowo <mr.apry.prastowo@gmail.com>
2018-06-27 20:53:25 +0700

    Update limit

--- src/main/java/com/psdkp/api/address/controller/DistrictController.java
+++ src/main/java/com/psdkp/api/address/controller/DistrictController.java
@@ -19,7 +19,7 @@ public class DistrictController {
     public Object getFindByName(
             @RequestParam(defaultValue = "", required = false) String name,
             @RequestParam(defaultValue = "", required = false) Integer id,
-            @PageableDefault(sort = { "id" }, value = 7100) Pageable pageable
+            @PageableDefault(sort = { "id" }, value = Integer.MAX_VALUE) Pageable pageable
     ) {
         if (id != null) {
             return districtService.findById(id);
